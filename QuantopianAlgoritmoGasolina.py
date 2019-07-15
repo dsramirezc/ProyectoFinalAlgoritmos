eef4d69c9bfefe8c88f9ffd548afc2e1dc3e622c
dsramirezc <dsramirezc@unal.edu.co>
2019-07-15 00:08:24 -0500

    Add files via upload
    Algoritmo de gasolina

---
+++ QuantopianAlgoritmoGasolina.py
@@ -0,0 +1,149 @@
+
+import numpy as np
+import scipy as sp
+from quantopian.algorithm import order_optimal_portfolio
+import quantopian.optimize as opt
+
+def initialize(context):
+    # Get continuous futures for Light Sweet Crude Oil...
+    #Variables añadidas para mejorar algoritmo
+    context.max_leverage = 0.95
+    context.max_pos_size = 0.02
+    context.max_turnover = 0.9
+    context.crude_oil = continuous_future('CL', roll='calendar')
+    # ... and RBOB Gasoline
+    context.gasoline = continuous_future('XB', roll='calendar')
+    
+    # Long and short moving average window lengths     
+    context.long_ma = 65
+    context.short_ma = 5
+    
+    # True if we currently hold a long position on the spread
+    context.currently_long_the_spread = False
+    # True if we currently hold a short position on the spread
+    context.currently_short_the_spread = False
+    
+    # Rebalance pairs every day, 30 minutes after market open
+    schedule_function(func=rebalance_pairs, 
+                      date_rule=date_rules.every_day(), 
+                      time_rule=time_rules.market_open(minutes=30))
+    
+    # Record Crude Oil and Gasoline Futures prices everyday
+    schedule_function(record_price, 
+                      date_rules.every_day(), 
+                      time_rules.market_open())
+
+def rebalance_pairs(context, data):
+    # Calculate how far away the current spread is from its equilibrium
+    zscore = calc_spread_zscore(context, data)
+    
+    # Get target weights to rebalance portfolio
+    target_weights = get_target_weights(context, data, zscore)
+    constrain_pos_size = opt.PositionConcentration.with_equal_bounds(
+            -context.max_pos_size,
+            context.max_pos_size
+        )
+
+        # Constrain target portfolio's leverage
+    max_leverage=opt.MaxGrossExposure(context.max_leverage)
+
+        # Ensure long and short books
+        # are roughly the same size
+    dollar_neutral = opt.DollarNeutral()
+
+        # Constrain portfolio turnover
+    max_turnover = opt.MaxTurnover(context.max_turnover)
+
+        # Constrain target portfolio's risk exposure
+        # By default, max sector exposure is set at
+        # 0.2, and max style exposure is set at 0.4
+
+
+    if target_weights:
+        # If we have target weights, rebalance portfolio
+        order_optimal_portfolio(
+            opt.TargetWeights(target_weights),
+            constraints=[
+                dollar_neutral ,
+ 
+            ]
+        )
+
+def calc_spread_zscore(context, data):
+    # Get pricing data for our pair of continuous futures
+    prices = data.history([context.crude_oil, 
+                           context.gasoline], 
+                          'price', 
+                          context.long_ma, 
+                          '1d')
+    
+    cl_price = prices[context.crude_oil]
+    xb_price = prices[context.gasoline]
+        
+    # Calculate returns for each continuous future  
+    cl_returns = cl_price.pct_change()[1:]
+    xb_returns = xb_price.pct_change()[1:]
+    
+    # Calculate the spread
+    regression = sp.stats.linregress(
+        xb_returns[-context.long_ma:],
+        cl_returns[-context.long_ma:],
+    )
+    spreads = cl_returns - (regression.slope * xb_returns)
+
+    # Calculate zscore of current spread
+    zscore = (np.mean(spreads[-context.short_ma]) - np.mean(spreads)) / np.std(spreads, ddof=1)
+
+    return zscore
+
+def get_target_weights(context, data, zscore):
+    # Get current contracts for both continuous futures
+    cl_contract, xb_contract = data.current(
+        [context.crude_oil, context.gasoline], 
+        'contract'
+    )
+    
+    # Initialize target weights
+    target_weights = {}  
+
+    if context.currently_short_the_spread and zscore < 0.0:
+        # Update target weights to exit position
+        target_weights[cl_contract] = 0
+        target_weights[xb_contract] = 0
+
+        context.currently_long_the_spread = False
+        context.currently_short_the_spread = False
+
+    elif context.currently_long_the_spread and zscore > 0.0:
+        # Update target weights to exit position
+        target_weights[cl_contract] = 0
+        target_weights[xb_contract] = 0
+
+        context.currently_long_the_spread = False
+        context.currently_short_the_spread = False
+
+    elif zscore < -1.0 and (not context.currently_long_the_spread):
+        # Update target weights to long the spread
+        target_weights[cl_contract] = 0.5
+        target_weights[xb_contract] = -0.5
+        
+        context.currently_long_the_spread = True
+        context.currently_short_the_spread = False
+
+    elif zscore > 1.0 and (not context.currently_short_the_spread):
+        # Update target weights to short the spread
+        target_weights[cl_contract] = -0.5
+        target_weights[xb_contract] = 0.5
+
+        context.currently_long_the_spread = False
+        context.currently_short_the_spread = True
+
+    return target_weights
+
+def record_price(context, data):
+    # Get current price of primary crude oil and gasoline contracts.
+    crude_oil_price = data.current(context.crude_oil, 'price')
+    gasoline_price = data.current(context.gasoline, 'price')
+      
+    # Adjust price of gasoline (42x) so that both futures have same scale.
+    record(Crude_Oil=crude_oil_price, Gasoline=gasoline_price*42)
